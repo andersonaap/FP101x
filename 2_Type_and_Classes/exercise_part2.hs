7dd48faa29cc89a588c692027770fb5be9a1aca0
Anderson Pavia <andersonaap@hotmail.com>
2014-10-24 10:07:12 -0200

    exercise chapter 2 part 2

---
+++ 2_Type_and_Classes/exercise_part2.hs
@@ -0,0 +1,32 @@
+{-
+define map and filter in terms of foldr
+-}
+
+map'    f = foldr (\x xs -> f (x:xs)) []
+filter' f = foldr (\x xs -> if f x then x:xs else xs) []
+
+
+map'' f    = foldr g []
+  where g x xs = f (x:xs)
+
+filter'' f = foldr g []
+  where g  x xs | f x       = x:xs
+                | otherwise = xs
+
+
+
+
+sum'' :: Num a => [a] -> a
+sum'' []     = 0
+sum'' (x:xs) = x + sum'' xs
+
+reverse'' []     = []
+reverse'' (x:xs) = (reverse xs) ++ [x]
+
+sum'      = foldr (+)  0
+product'  = foldr (*)  1
+and'      = foldr (&&) True
+length'   = foldr (\_ xs -> xs + 1) 0
+reverse'  = foldr (\x xs -> xs ++ [x]) []
+(+++) ys  = foldr (:) ys
+
