77ae7f152f28a7f4cb12c45de8a27e75c890c484
Anderson Pavia <andersonaap@hotmail.com>
2014-10-27 11:29:18 -0200

    exercise chapter 2 part 2, f#

---
+++ 2_Type_and_Classes/exercise_part2.fs
@@ -0,0 +1,13 @@
+﻿
+
+let map    f ys = List.foldBack (fun x xs -> (f x)::xs )                  ys []
+let filter f ys = List.foldBack (fun x xs -> if (f x) then x::xs else xs) ys []
+
+
+
+let numeros    = [1..6]
+let pares      = filter (fun x -> x % 2 = 0) numeros
+let quadradros = map    (fun x -> x * x)     numeros
+
+printfn "%A" pares
+printfn "%A" quadradros
