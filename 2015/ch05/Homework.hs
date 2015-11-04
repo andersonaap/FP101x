606f04c69b7209a65ec878c5fac754ae32fed5a2
andersonaap <andersonaap@hotmail.com>
2015-11-03 23:52:11 -0200

    Homework ch05

--- 2015/ch05/Homework.hs
+++ 2015/ch05/Homework.hs
@@ -123,16 +123,16 @@
 --  | otherwise = elem x ys
 
 -- #9
-merge :: Ord a => [a] -> [a] -> [a]
-merge [] ys = ys
-merge xs [] = xs
-merge (x:xs) (y:ys) = if x < y then x : merge xs (y:ys) else y : merge (x:xs) ys 
+--merge :: Ord a => [a] -> [a] -> [a]
+--merge [] ys = ys
+--merge xs [] = xs
+--merge (x:xs) (y:ys) = if x < y then x : merge xs (y:ys) else y : merge (x:xs) ys 
 
--- #10
-halve xs = splitAt (length xs `div` 2) xs
+---- #10
+--halve xs = splitAt (length xs `div` 2) xs
 
-msort :: Ord a => [a] -> [a]
-msort [] = []
-msort [x] = [x]
-msort xs = merge (msort ys) (msort zs)
-  where (ys, zs) = halve xs
+--msort :: Ord a => [a] -> [a]
+--msort [] = []
+--msort [x] = [x]
+--msort xs = merge (msort ys) (msort zs)
+--  where (ys, zs) = halve xs
